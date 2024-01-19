c7c04a1c698e0fdb4e98b8ccbce20aebabfc6890
austinbarish <abb110@georgetown.edu>
2024-01-19 11:12:51 -0500

    Increased Categories / Better Highlighting / Greens for prices

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import json
 import time
 import plotly.express as px
 import plotly.graph_objects as go
+import seaborn as sns
 from shiny import render, ui, App, reactive
 from shinywidgets import output_widget, render_widget
 import shinyswatch
@@ -20,8 +21,10 @@ df = pd.read_csv(
 
 # Create Color Map
 # Done outside the function to remain consistent
-colors = px.colors.qualitative.Light24
-color_map = {cat: colors[i] for i, cat in enumerate(df.main_category.unique())}
+# Create a color palette with as many colors as there are categories
+custom_palette = sns.color_palette("husl", len(df.main_category.unique())).as_hex()
+color_map = {cat: custom_palette[i] for i, cat in enumerate(df.main_category.unique())}
+
 
 # Create a dict of all the categories
 categories = {cat: cat for cat in df.categories.explode().unique()}
@@ -141,46 +144,50 @@ def data_filterer(
     prices=["$", "$$", "$$$", "$$$$"],
     main_category=["All Categories"],
 ):
-    # Filter the dataframe by rating and review count
-    df = df[
-        (df.rounded_normalized_rating >= rating_range[0])
-        & (df.rounded_normalized_rating <= rating_range[1])
-        & (df.total_reviews >= review_range[0])
-        & (df.total_reviews <= review_range[1])
-    ]
+    try:
+        # Filter the dataframe by rating and review count
+        df = df[
+            (df.rounded_normalized_rating >= rating_range[0])
+            & (df.rounded_normalized_rating <= rating_range[1])
+            & (df.total_reviews >= review_range[0])
+            & (df.total_reviews <= review_range[1])
+        ]
 
-    # Add price codes
-    price_map = {"$": 1, "$$": 2, "$$$": 3, "$$$$": 4}
-    df["price_code"] = df.price.map(price_map)
+        # Add price codes
+        price_map = {"$": 1, "$$": 2, "$$$": 3, "$$$$": 4}
+        df["price_code"] = df.price.map(price_map)
 
-    prices_codes = [price_map[price] for price in prices]
+        prices_codes = [price_map[price] for price in prices]
 
-    # Filter the dataframe by price
-    df = df[df.price_code.isin(prices_codes)]
+        # Filter the dataframe by price
+        df = df[df.price_code.isin(prices_codes)]
 
-    if "All Categories" not in main_category:
-        df = df[df.main_category.isin(main_category)]
+        if "All Categories" not in main_category:
+            df = df[df.main_category.isin(main_category)]
 
-    # Sort by main category so it is alphabetical
-    df = df.sort_values(by=["main_category"])
+        # Sort by main category so it is alphabetical
+        df = df.sort_values(by=["main_category"])
 
-    from sklearn.preprocessing import MinMaxScaler
-    import math
+        from sklearn.preprocessing import MinMaxScaler
+        import math
 
-    # Calculate raw score
-    df["score"] = (df["normalized_rating"] * 10) * (
-        df["normalized_total_reviews"] / 100
-    )
+        # Calculate raw score
+        df["score"] = (df["normalized_rating"] * 10) * (
+            df["normalized_total_reviews"] / 100
+        )
 
-    # Apply logarithmic transformation to score
-    df["score"] = df["score"].apply(lambda x: 0 if x == 0 else math.log(x + 1))
+        # Apply logarithmic transformation to score
+        df["score"] = df["score"].apply(lambda x: 0 if x == 0 else math.log(x + 1))
 
-    # Use MinMaxScaler for normalization
-    scaler = MinMaxScaler()
-    df["score"] = scaler.fit_transform(df[["score"]]) * 100
+        # Use MinMaxScaler for normalization
+        scaler = MinMaxScaler()
+        df["score"] = scaler.fit_transform(df[["score"]]) * 100
 
-    # Round the score
-    df["score"] = df["score"].round(2)
+        # Round the score
+        df["score"] = df["score"].round(2)
+
+    except:
+        df = pd.DataFrame()
 
     return df
 
@@ -238,12 +245,20 @@ def create_grid(
     if list(highlighted_restaurants) != []:
         try:
             # Create Color Map for Highlighted Restaurants
-            df["Highlighted"] = [
-                name if name in highlighted_restaurants else "Other" for name in df.name
-            ]
+            df["Highlighted"] = np.where(
+                df["name"].isin(highlighted_restaurants), df["name"], "Other"
+            )
             highlighted_color_map = {
                 name: "highlight" for name in enumerate(df.Highlighted.unique())
             }
+
+            # Set the size for highlighted and non-highlighted datapoints
+            highlighted_size = 10
+            non_highlighted_size = 2
+
+            df["highlighted_size"] = np.where(
+                df["Highlighted"] == "Other", non_highlighted_size, highlighted_size
+            )
 
             # Change Other to Gray
             highlighted_color_map["Other"] = "gray"
@@ -258,6 +273,8 @@ def create_grid(
             y="normalized_total_reviews",
             color="Highlighted",
             color_discrete_map=highlighted_color_map,
+            size="highlighted_size",
+            size_max=highlighted_size,
             hover_name="name",
             custom_data=[
                 "name",
@@ -267,17 +284,25 @@ def create_grid(
                 "total_reviews",
             ],
             title="DC Restaurant Grid",
-            labels={"main_category": "Category"},
+            labels={"Highlighted": "Highlighted Restaurants"},
             # text='name'
         )
 
+    # If not highlighted:
     else:
         if coloring == "price":
+            # Sort by price to be $, $$, $$$, $$$$
+            df = df.sort_values(by=["price"])
+
+            # Get the Greens color scale from Plotly
+            greens = px.colors.sequential.Greens
+
+            # Create Color Map for Price
             price_color_map = {
-                "$": px.colors.qualitative.Light24[8],  # Green
-                "$$": px.colors.qualitative.Light24[6],  # Yellow
-                "$$$": px.colors.qualitative.Light24[7],  # Orange
-                "$$$$": px.colors.qualitative.Light24[0],  # Red
+                "$": greens[2],  # Light Green
+                "$$": greens[4],  # Medium Green
+                "$$$": greens[6],  # Dark Green
+                "$$$$": greens[8],  # Darkest Green
             }
 
             # Create Scatter
@@ -352,7 +377,7 @@ def create_grid(
 
     # Update the hover template to include only 'name' and 'price'
     fig.update_traces(
-        hovertemplate="<b><a href='%{customdata[5]}' style='text-decoration: underline; color: inherit;'>%{hovertext}</a></b><br>Price: %{customdata[1]}<br>Rating: %{customdata[2]:.2f}<br>Normalized Rating: %{customdata[3]}<br>Review Count: %{customdata[4]:,}<br>Score: %{customdata[6]:.2f}"
+        hovertemplate="<b>%{hovertext}</b><br>Price: %{customdata[1]}<br>Rating: %{customdata[2]:.2f}<br>Normalized Rating: %{customdata[3]}<br>Review Count: %{customdata[4]:,}<br>Score: %{customdata[6]:.2f}"
     )
 
     # Add quadrant Lines
@@ -440,6 +465,9 @@ def server(input, output, session):
                 "score",
             ]
         ]
+
+        # Round average rating to 3 decimal places
+        df_filtered["average_rating"] = df_filtered["average_rating"].round(3)
 
         # Remove underscores
         df_filtered.columns = [col.replace("_", " ") for col in df_filtered.columns]
